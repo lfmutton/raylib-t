9bc44a8ce090877c696c90a8097a7fe99cf09d76
lfmutton <lfmutton@gmail.com>
2024-06-22 03:20:41 -0300

    Update in the lines

--- main.c
+++ main.c
@@ -1,40 +1,18 @@
-/*******************************************************************************************
- *
- *   raylib [core] example - Basic window
- *
- *   Welcome to raylib!
- *
- *   To test examples, just press F6 and execute raylib_compile_execute script
- *   Note that compiled executable is placed in the same folder as .c file
- *
- *   You can find all basic examples on C:\raylib\raylib\examples folder or
- *   raylib official webpage: www.raylib.com
- *
- *   Enjoy using raylib. :)
- *
- *   Example originally created with raylib 1.0, last time updated with raylib 1.0
- *
- *   Example licensed under an unmodified zlib/libpng license, which is an OSI-certified,
- *   BSD-like license that allows static linking with closed source software
- *
- *   Copyright (c) 2013-2023 Ramon Santamaria (@raysan5)
- *
- ********************************************************************************************/
-
 #include "raylib.h"
 
+void GridLines(int x, int y);
 //------------------------------------------------------------------------------------
 // Program main entry point
 //------------------------------------------------------------------------------------
+int i;
+
 int main(void)
 {
     // Initialization
     //--------------------------------------------------------------------------------------
     const int screenWidth = 800;
     const int screenHeight = 600;
-    int i;
     Vector2 mousePos;
-
     InitWindow(screenWidth, screenHeight, "Testing");
     HideCursor();
 
@@ -53,17 +31,11 @@ int main(void)
         //----------------------------------------------------------------------------------
         BeginDrawing();
         ClearBackground(BLACK);
-        for (i = 0; i < screenWidth; i += 50)
-        {
-            DrawLine(i, 0, i, screenHeight, WHITE);
-        }
-        for (i = 0; i < screenHeight; i += 50)
-        {
-            DrawLine(0, i, screenWidth, i, WHITE);
-        }
-        DrawCircle(screenWidth / 2, screenHeight / 2, 10, WHITE);
+        GridLines(screenWidth, screenHeight);
+        DrawLine(GetMouseX(), GetMouseY(), screenWidth / 2, screenHeight / 2, RED);
 
-        DrawCircleV(mousePos, 10, WHITE);
+        DrawCircle(screenWidth / 2, screenHeight / 2, 8, WHITE);
+        DrawCircleV(mousePos, 8, WHITE);
 
         EndDrawing();
         //----------------------------------------------------------------------------------
@@ -75,4 +47,16 @@ int main(void)
     //--------------------------------------------------------------------------------------
 
     return 0;
+}
+
+void GridLines(int x, int y)
+{
+    for (i = 0; i < x; i += 50)
+    {
+        DrawLine(i, 0, i, y, WHITE);
+    }
+    for (i = 0; i < y; i += 50)
+    {
+        DrawLine(0, i, x, i, WHITE);
+    }
 }
